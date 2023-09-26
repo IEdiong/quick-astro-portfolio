504694b9b5709e2f62d4dea4bb8bfa0b7c75d9b0
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-26 13:15:14 +0100

    basic info added

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,122 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  baseUrl: 'https://iediong.vercel.app',
+  name: 'Ediongsenyene Joseph I.',
+  jobDescription: 'Web Developer',
+  about: `
+   I'm a Software Engineer with expertise in both Frontend and Backend development. My proficiency includes TypeScript, JavaScript, ReactJS, NextJS, Angular, C#, ASP.NET, and I have hands-on experience with technologies such as Bootstrap, TailwindCSS and Chakra-UI. Holding a Bachelor's degree in Electrical/Electronics Engineering, I am enthusiastic about seeking opportunities to collaborate with seasoned software engineers, driven by a relentless determination to learn and adapt quickly while effectively sharing knowledge with others.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: 'HNG Internship (Remote)',
+      location: 'Nigeria',
+      startDate: 'Sep 2023',
+      endDate: 'Present',
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        '- Developing screens and UI components for the web application using NextJs, Chakra-UI and Tailwind.',
+        '- Build APIs for web and mobile applications using C#/ASP.NET, EF Core and MySQL.',
+      ],
+    },
+    {
+      name: 'nHub Nigeria (NYSC)',
+      location: 'Jos, Nigeria',
+      startDate: 'Jul 2022',
+      endDate: 'May 2023',
+      description: [
+        '- Teaching interns and students how to use Front-end technologies including HTML/CSS, Javascript, DOM Manipulation, Bootstrap, TailwindCSS, React, Chakra-UI, Typescript, State Management, NextJs etc.',
+        '- Building and leading the team in building some in-house products.',
+        '- Code Improvements and Refactoring .',
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: 'Vnicom Solutions Limited (Remote)',
+      location: 'Magboro, Ogun, Nigeria',
+      startDate: 'Apr 2023',
+      endDate: 'Aug 2023',
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        '- Learned how to build back-end applications with C#, ASP.NET Core, Entity Framework Core, MySQL and Bootstrap',
+        '- Built an online platform for purchasing books using the above mentioned technologies',
+        '- Built a student management system to keep track of all interns across all programs',
+      ],
+    },
+    {
+      name: 'Google Africa Scholarship (Remote)',
+      location: 'Nigeria',
+      startDate: 'Apr 2021',
+      endDate: 'Feb 2022',
+      description: [
+        '- Learnt to use Typescript with Angular (a JavaScript framework) to build Frontend applications',
+        '- Learnt about RxJs and consuming APIs in Angular',
       ],
     },
 
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
-    },
+    // {
+    //   name: 'Interns Pakistan',
+    //   location: 'Pakistan',
+    //   startDate: 'Oct 2020',
+    //   endDate: 'Nov 2020',
+    //   description: [
+    //     "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+    //     'The internship track is built by an industry expert that let develop the skills in the areas most important ',
+    //   ],
+    // },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: 'University of Uyo',
+      location: 'Uyo, Akwa Ibom, Nigeria',
+      startDate: '2012',
+      endDate: '2021',
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        'Bachelor of Engineering - Electrical/Electronics Engineering',
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    twitter: 'https://twitter.com/IEdiong',
+    github: 'https://github.com/IEdiong',
+    email: 'mailto:ediongi6@hotmail.com',
+    linkedin: 'https://www.linkedin.com/in/iediong/',
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: 'Movie-Box',
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: '/assets/images/movie-box.png',
+      githubUrl: 'https://github.com/IEdiong/movie-box',
+      liveUrl: 'https://iediong-movie-box.vercel.app/',
+    },
+    {
+      title: 'Audiophile e-commerce',
+      isFeatured: true,
+      thumbnail: '/assets/images/audiophile-ecommerce.jpg',
+      githubUrl: 'https://github.com/IEdiong/aph-e-commerce',
+      liveUrl: 'https://aph-e-commerce.vercel.app/',
+    },
+    {
+      title: 'Insure Landing Page',
+      isFeatured: true,
+      thumbnail: '/assets/images/insure-landing-page.jpg',
+      githubUrl: 'https://github.com/IEdiong/fem-insure-landing-page',
+      liveUrl: 'https://iediong.github.io/fem-insure-landing-page/',
+    },
+    {
+      title: 'Todo App',
+      isFeatured: true,
+      thumbnail: '/assets/images/todo-app.jpg',
+      githubUrl: 'https://github.com/IEdiong/fem/tree/main/todo-app',
+      liveUrl: 'https://iediong-todo-app.netlify.app/',
+    },
+    {
+      title: 'Testimonials Grid Section',
+      isFeatured: false,
+      thumbnail: '/assets/images/testimonials-grid-section.jpg',
+      githubUrl: 'https://github.com/IEdiong/fem-testimonials-grid-section',
+      liveUrl: 'https://iediong.github.io/fem-testimonials-grid-section/',
     },
   ],
 };
