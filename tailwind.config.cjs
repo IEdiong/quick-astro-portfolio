af461893b99c1a0457acde0a7c2f4edfec32c308
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-03-30 21:10:54 +0100

    style: fix layout ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        dark: "#111111",
-        light: "#FFFFFF",
-        blue: "#33d2ff",
-        gray: "#C4C4C4",
-        "dark-gray": "#1A1A1A",
+        dark: '#111111',
+        light: '#FFFFFF',
+        blue: '#33d2ff',
+        gray: '#C4C4C4',
+        'dark-gray': '#1A1A1A',
       },
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        outfit: ["Outfit", "sans-serif"],
+        inter: ['Inter', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
       },
+    },
+    screens: {
+      sm: '744px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [],
